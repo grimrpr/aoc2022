f8dc8fbb32586dfeed668d4fc0b0c7bf78f0f79b
grimrpr <benjamin.aschenbrenner@gmail.com>
2023-11-26 10:44:39 +0100

    Add day7 results

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -2,7 +2,7 @@ use std::{
     fs::File,
     io::BufRead,
     io::BufReader,
-    path::{Path, PathBuf}, env::current_dir,
+    path::{Path, PathBuf},
 };
 
 #[derive(Debug)]
@@ -18,10 +18,29 @@ enum DirElem {
 }
 
 impl DirElem {
-    pub fn get_name(&self) -> &str {
+    fn get_name(&self) -> &str {
         match &self {
             DirElem::DirNode { name, .. } => name,
             DirElem::FileNode { name, .. } => name,
+        }
+    }
+
+    fn get_sizes(&self) -> (isize, Vec<(String, isize)>) {
+        match self {
+            DirElem::FileNode { size, .. } => (*size, Vec::new()),
+            DirElem::DirNode { name, contents } => {
+                let (total, mut subdirs) = contents.iter().fold(
+                    (0, Vec::new()),
+                    |(total_size, mut dirs), node| match node.get_sizes() {
+                        (node_size, node_dirs) => {
+                            dirs.extend(node_dirs);
+                            (total_size + node_size, dirs)
+                        }
+                    },
+                );
+                subdirs.push((name.to_owned(), total));
+                (total, subdirs)
+            }
         }
     }
 }
@@ -57,8 +76,7 @@ impl DirTree {
         path.as_ref()
             .iter()
             .try_fold(&mut self.root, |curr_dir, next_dir| match curr_dir {
-                _ if curr_dir.get_name() == next_dir.to_str().unwrap() => {
-                    Some(curr_dir)},
+                _ if curr_dir.get_name() == next_dir.to_str().unwrap() => Some(curr_dir),
                 DirElem::DirNode { contents, .. } => {
                     contents.into_iter().find(|x| x.get_name() == next_dir)
                 }
@@ -115,7 +133,8 @@ where
                             &path,
                         )
                         .ok();
-                        (tree, path)},
+                        (tree, path)
+                    }
                     _ => (tree, path),
                 }
             },
@@ -123,8 +142,47 @@ where
         .0
 }
 
+fn dirs_below_limit_size(size_limit: isize, dir_sizes: &Vec<(String, isize)>) -> isize {
+    dir_sizes
+        .iter()
+        .filter_map(|(_, dir_size)| {
+            if dir_size <= &size_limit {
+                Some(dir_size)
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+fn dir_size_to_be_removed(space_required: isize, dir_sizes: &Vec<(String, isize)>) -> isize {
+    dir_sizes
+        .iter()
+        .filter_map(|(_, dir_size)| {
+            if dir_size >= &space_required {
+                Some(dir_size)
+            } else {
+                None
+            }
+        })
+        .min()
+        .unwrap()
+        .clone()
+}
+
 pub fn print_answer() {
-    let mut buf_read = BufReader::new(File::open("data/input_day7").unwrap());
+    let buf_read = BufReader::new(File::open("data/input_day7").unwrap());
     let file_tree = parse_dirs(buf_read);
     //println!("Parsed tree: {:?}", file_tree);
+    let dir_sizes = file_tree.root.get_sizes();
+    let limit = 100_000;
+    let size = dirs_below_limit_size(limit, &dir_sizes.1);
+    println!("Size of dirs not larger than {} is: {}", limit, size);
+    // part 2
+    let space_min_to_be_freed = 30_000_000 - (70_000_000 - dir_sizes.0);
+    let remove_size = dir_size_to_be_removed(space_min_to_be_freed, &dir_sizes.1);
+    println!(
+        "Size of dir closest to required {} is: {}",
+        space_min_to_be_freed, remove_size
+    );
 }
